2ca3f24d6866895a86d3733c4bd1adbd37b5c735
kazuhira-r <kazuhira.m@gmail.com>
2022-06-12 20:41:33 +0900

    add, WildFly Hot Rod http session management examle

---
+++ remote-wildfly-hotrod-session/src/main/java/org/littlewings/infinispan/session/CounterServlet.java
@@ -0,0 +1,28 @@
+package org.littlewings.infinispan.session;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/counter")
+public class CounterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+
+        Counter counter = (Counter) session.getAttribute("counter");
+
+        if (counter == null) {
+            counter = new Counter();
+            session.setAttribute("counter", counter);
+        }
+
+        int current = counter.increment();
+
+        resp.getWriter().write(String.format("current value = %d%n", current));
+    }
+}
